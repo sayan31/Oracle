51188720efb956d268f578baf55dbfbb15c2dd4a
Sayan Guha <sayanguha99@gmail.com>
2019-04-03 20:51:49 +0530

    Interim Commit: Assignment E, Step 7

---
+++ adf-topgear/assignment-e/ViewController/src/com/sayan/adf/view/backing/backin_login.java
@@ -0,0 +1,18 @@
+package com.sayan.adf.view.backing;
+
+import oracle.adf.view.rich.component.rich.input.RichInputText;
+
+public class backin_login {
+    private RichInputText password;
+
+    public backin_login() {
+    }
+
+    public void setPassword(RichInputText password) {
+        this.password = password;
+    }
+
+    public RichInputText getPassword() {
+        return password;
+    }
+}
